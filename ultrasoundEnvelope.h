e78fa24d2ab0525959578303c938dbd3a032e048
ruiesteves-pt <56549616+ruiesteves-pt@users.noreply.github.com>
2023-11-17 14:25:39 +0100

    Update ultrasoundEnvelope.h

--- ultrasoundEnvelope.h
+++ ultrasoundEnvelope.h
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <complex>
-#include <vector>
-#include <cmath>
+#pragma once
 
+#include <vector>
+#include <complex>
+
+// Constant for Pi, if not already defined
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-using namespace std;
-
 // This function performs envelope detection on beamformed ultrasound RF data.
-// It follows the sequence:
+// Parameters:
+// - data: A vector of doubles representing the input signal.
+// The function follows this sequence:
 //		1. FFT via discrete Fourier transform (DFT)
 //		2. Removing the negative frequencies
 //		3. Inverse FFT via inverse discrete Fourier transform (IDFT)
-//		4. Absolute value of obtained signal
+//		4. Absolute value of the obtained signal
+// Returns:
+// - A vector of doubles representing the envelope of the input signal.
 
-vector<double> ultrasoundEnvelope(vector<double> data);
+std::vector<double> ultrasoundEnvelope(const std::vector<double>& data);

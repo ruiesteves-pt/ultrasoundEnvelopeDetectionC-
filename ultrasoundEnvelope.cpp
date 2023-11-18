dbd0fe1c0723363ebfebc47d93327639aaad6776
ruiesteves-pt <56549616+ruiesteves-pt@users.noreply.github.com>
2023-11-18 13:29:30 +0100

    Update ultrasoundEnvelope.cpp

--- ultrasoundEnvelope.cpp
+++ ultrasoundEnvelope.cpp
@@ -1,64 +1,51 @@
 #include "ultrasoundEnvelope.h"
+#include <vector>
+#include <complex>
+#include <cmath>
 
 using namespace std;
 
 // Manual FFT function
 vector<complex<double>> manualFFT(const vector<double>& data) {
-    complex<double> i(0, 1.0);
-
-    double nSamples = data.size();
+    size_t nSamples = data.size();
     vector<complex<double>> fftData(nSamples, 0);
+    double twoPiOverN = 2.0 * M_PI / nSamples;
 
-    for (double k = 0; k < nSamples; ++k) {
-        for (double n = 0; n < nSamples; ++n) {
-            fftData[k] += data[n] * exp(-complex<double>(0.0, 1.0) * 2.0 * M_PI * k * n / nSamples);
+    for (size_t k = 0; k < nSamples; ++k) {
+        for (size_t n = 0; n < nSamples; ++n) {
+            double angle = twoPiOverN * k * n;
+            fftData[k] += data[n] * exp(complex<double>(0.0, -angle));
         }
     }
 
     return fftData;
 }
 
-
 // Manual IFFT function
 vector<complex<double>> manualIFFT(const vector<complex<double>>& data) {
-    complex<double> i(0, 1.0);
-    double nSamples = data.size();
+    size_t nSamples = data.size();
     vector<complex<double>> ifftData(nSamples, 0);
+    double twoPiOverN = 2.0 * M_PI / nSamples;
 
-    for (double n = 0; n < nSamples; ++n) {
-        for (double k = 0; k < nSamples; ++k) {
-            ifftData[n] += (1.0 / nSamples) * data[k] * exp(i * 2.0 * M_PI * k * n / nSamples);
+    for (size_t n = 0; n < nSamples; ++n) {
+        for (size_t k = 0; k < nSamples; ++k) {
+            double angle = twoPiOverN * k * n;
+            ifftData[n] += (1.0 / nSamples) * data[k] * exp(complex<double>(0.0, angle));
         }
     }
 
     return ifftData;
 }
 
-vector<double> ultrasoundEnvelope(vector<double> data) {
-    double nSamples = data.size();
+vector<double> ultrasoundEnvelope(const vector<double>& data) {
+    size_t nSamples = data.size();
      
     // FFT step
     vector<complex<double>> fftData = manualFFT(data);
 
     // Removing negative frequencies
-    vector<double> freqPos(nSamples - 2);
-    vector<double> freqNeg(nSamples - 2);
-
-    for (int k = 2; k < nSamples; ++k) {
-        if (k <= nSamples / 2) {
-            freqPos[k - 2] = k;
-        }
-        else {
-            freqNeg[k - 2] = k;
-        }
-    }
-
-    for (int i = 0; i < freqPos.size(); ++i) {
-        fftData[freqPos[i]] = 2.0 * fftData[freqPos[i]];
-    }
-
-    for (int i = 0; i < freqNeg.size(); ++i) {
-        fftData[freqNeg[i]] = 0.0;
+    for (size_t k = nSamples / 2; k < nSamples; ++k) {
+        fftData[k] = 0.0;
     }
 
     // IFFT step
@@ -66,10 +53,9 @@ vector<double> ultrasoundEnvelope(vector<double> data) {
 
     // Take the absolute value
     vector<double> envelope(nSamples);
-    for (int n = 0; n < nSamples; ++n) {
+    for (size_t n = 0; n < nSamples; ++n) {
         envelope[n] = abs(hilbertMan[n]);
     }
 
     return envelope;
-
 }
